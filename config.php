1abd0e82b13e889c532a420280bbdbcf9a8d1c76
amezer <jocelyn.her.ho@gmail.com>
2022-03-22 14:33:36 +0800

    added heroku clearDB connection

--- config.php
+++ config.php
@@ -1,13 +1,18 @@
 <?php 
     session_start();
     $current_userID = $_SESSION['id'];
-    $dbServername = "localhost";
-    $dbUsername = "root";
-    $dbPassword = "Z3(sz83Nva-nnYR9";
+    $dbServername = "us-cdbr-east-05.cleardb.net";
+    $dbUsername = "b29c6cdf9ed757";
+    $dbPassword = "955082fb";
+    $database = "heroku_8839b7520284fd2";
+    // $dbUsername = "root";
+    // $dbPassword = "Z3(sz83Nva-nnYR9";
+
+    //mysql://b29c6cdf9ed757:955082fb@us-cdbr-east-05.cleardb.net/heroku_8839b7520284fd2?reconnect=true
 
     try{
         $current_userID = $_SESSION['id'];
-        $conn = new PDO("mysql:host=$dbServername;dbname=photo_sharing_app", $dbUsername, $dbPassword);
+        $conn = new PDO("mysql:host=$dbServername;dbname=$database", $dbUsername, $dbPassword);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }catch(PDOException $e){
         print("Error: " . $sql . "<br>" . $e->getMessage());
