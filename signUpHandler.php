1885e1f5457d9ed176e6e43ea18befe0613b6f40
amezer <jocelyn.her.ho@gmail.com>
2022-04-15 14:15:36 +0800

    debug

--- signUpHandler.php
+++ signUpHandler.php
@@ -22,28 +22,8 @@
         $bio = addslashes($_POST['bio']);
         $hashpw = password_hash($pw, PASSWORD_BCRYPT);
 
-        // require("config.php"); //connect to db
+        require("config.php"); //connect to db
 
-        echo "connection starts here";
-        session_start();
-    $current_userID = $_SESSION['id'];
-    $dbServername = "us-cdbr-east-05.cleardb.net";
-    $dbUsername = "b29c6cdf9ed757";
-    $dbPassword = "955082fb";
-    $database = "heroku_8839b7520284fd2";
-    // $dbUsername = "root";
-    // $dbPassword = "Z3(sz83Nva-nnYR9";
-
-    //mysql://b29c6cdf9ed757:955082fb@us-cdbr-east-05.cleardb.net/heroku_8839b7520284fd2?reconnect=true
-
-    try{
-        $current_userID = $_SESSION['id'];
-        $conn = new PDO("mysql:host=$dbServername;dbname=$database", $dbUsername, $dbPassword);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "connected";
-    }catch(PDOException $e){
-        print("Error: " . $sql . "<br>" . $e->getMessage());
-    }
         $sql = $conn->prepare("SELECT Email FROM Users");
         $sql->execute();
 
